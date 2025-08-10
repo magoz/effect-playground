46d97964c5cf3a405e100bd165d0ec9e706caf27
magoz <apps@magoz.is>
2025-08-10 10:43:46 +0200

    add query

--- lib/services/db/run.ts
+++ lib/services/db/run.ts
@@ -1,10 +1,18 @@
 import { Effect } from 'effect'
 import { DrizzleClient } from './service'
+import { eq } from 'drizzle-orm'
+import { users } from './schema'
 
 const program = Effect.gen(function* () {
   const db = yield* DrizzleClient
-  const users = yield* db.query.users.findMany()
-  console.log('Users:', users)
+  const allUsers = yield* db.query.users.findMany()
+  console.log('Users:', allUsers)
+
+  const nonExistingUser = yield* db.query.users.findFirst({
+    where: eq(users.id, 999999)
+  })
+
+  console.log('Non existing user:', nonExistingUser)
 }).pipe(Effect.catchAll(error => Effect.logError(`❌ Error: ${error}`)))
 
 program.pipe(Effect.provide(DrizzleClient.Default), Effect.runPromise)
